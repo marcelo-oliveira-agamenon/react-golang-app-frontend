d50878cd7f52ce84c650cd8179c7549af8259c1e
Marcelo Oliveira <marcelo_alfahot@hotmail.com>
2020-07-16 22:07:43 -0300

    fixing merge error

--- readme.md
+++
@@ -1,1 +0,0 @@
-## Projeto de Comércio de Produtos
--- src/config/index.tsx
+++
@@ -1,6 +0,0 @@
-const envs = {
-  API_URL: "https://node-app--backend.herokuapp.com",
-  DEV_URL: "http://localhost:4000",
-};
-
-export default envs;
