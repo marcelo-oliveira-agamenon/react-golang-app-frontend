6653d494b74714d80c08008711d7274c9d89acdd
Marcelo Oliveira <marcelo_alfahot@hotmail.com>
2020-11-06 22:56:37 -0300

    WIP

--- web/src/config/axiosConfig.tsx
+++ web/src/config/axiosConfig.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://ecommerce-go.herokuapp.com",
-  //baseURL: "http://localhost:4000",
+  //baseURL: "http://localhost:4000"
 });
 
 export default api;
